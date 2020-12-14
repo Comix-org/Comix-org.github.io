60b60da1b66651cf35b47a3a6a232db90a6b48df
Comix-org <75125839+Comix-org@users.noreply.github.com>
2020-12-14 14:19:17 +0530

    Update magazineData.js

--- magazineData.js
+++ magazineData.js
@@ -1,4 +1,4 @@
-//Format to create new magazine:
+//Below is the format to create new magazine:
 /*
 	First in the "Magazines" folder create a new PDF file. Then insert code similar to the below example in the "magazineData" array.
 	{
