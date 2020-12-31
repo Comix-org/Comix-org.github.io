cd119dc36fbac2e9a04a0cc9a43c1f987fc51652
Comix-org <75125839+Comix-org@users.noreply.github.com>
2020-12-31 17:45:21 +0530

    Update whole website

--- magazineData.js
+++ magazineData.js
@@ -14,76 +14,89 @@ var magazineData = [
 		fileName: "XanterEp1.pdf",
 		issue: "1",
 		name: "Xanter - The Clumsy Antivirus",
+		series: "Xanter - The Clumsy Antivirus"
 	},
 	{
 		author: "Avaneesh",
 		fileName: "TheManAndTheDog.pdf",
 		issue: "1",
 		name: "The Man and The Dog",
+		series: "The Man and The Dog"
 	},
 	{
 		author: "Aahan",
 		fileName: "TheElephantAndTheDoctor.pdf",
-		issue: 1,
-		name: "Laughs - The Elephant & The Doctor"
+		issue: "1",
+		name: "Laughs - The Elephant & The Doctor",
+		series: "Laughs"
 	},
 	{
 		author: "Aahan",
 		fileName: "OctoInk.pdf",
 		issue: "2",
-		name: "Laughs - OctoInky"
+		name: "Laughs - OctoInk",
+		series: "Laughs"
 	},
 	{
 		author: "Aahan",
 		fileName: "SuperBoyEp1.pdf",
 		issue: "1",
-		name: "SuperBoy - Playing With A Thief"
+		name: "SuperBoy - Playing With A Thief",
+		series: "SuperBoy"
 	},
 	{
 		author: "Avaneesh",
 		fileName: "ManAndTheDog2.pdf",
 		issue: "2",
-		name: "The Man and The Dog"
+		name: "The Man and The Dog",
+		series: "The Man and The Dog"
 	},
 	{
 		author: "Aahan",
 		fileName: "SuperBoyEp2.pdf",
 		issue: "1",
-		name: "SuperBoy - Stealing Santa"
+		name: "SuperBoy - Stealing Santa",
+		series: "SuperBoy",
 	},
 	{
 		author: "Aahan",
 		fileName: "FractionsMixUp.pdf",
 		issue: "3",
-		name: "Laughs - Muddled Fractions"
+		name: "Laughs - Muddled Fractions",
+		series: "Laughs"
 	},
 	{
 		author: "Aahan",
 		fileName: "Covid-19@School.pdf",
 		issue: "4",
-		name: "Laughs - Covid@School"
+		name: "Laughs - Covid@School",
+		series: "Laughs"
 	},{
 		author: "Aahan",
 		fileName: "FullMarks.pdf",
 		issue: "5",
-		name: "Laughs - Full Marks!"
+		name: "Laughs - Full Marks!",
+		series: "Laughs"
 	},
 	{
 		author: "Avaneesh",
 		fileName: "SpecialEdition1.pdf",
 		issue: "1",
-		name: "DECEMBER SPECIAL EDITION"
+		name: "DECEMBER SPECIAL EDITION",
+		series: "Special Editons"
 	},
 	{
 		author: "Avaneesh",
 		fileName: "MiniMix.pdf",
 		issue: "1",
-		name: "MiniMix - Sleepy Computers"
+		name: "MiniMix - Sleepy Computers",
+		series: "MiniMix"
 	},
 	{
 		author: "Aahan",
 		fileName: "PartsOfTheComputer.pdf",
 		issue: "6",
-		name: "Laughs - Parts of the Computer"
+		name: "Laughs - Parts of the Computer",
+		series: "Laughs"
 	}
 ];
--- script.js
+++ script.js
@@ -1,25 +1,69 @@
+//Some localStorage stuff
 if(localStorage.getItem("MyFavoriteComix") === null){
 	localStorage.setItem("MyFavoriteComix", "[]");
 }
 var favorited = JSON.parse(localStorage.getItem("MyFavoriteComix"));
-for(var i = 0; magazineData.length; i++){
+
+
+
+//Render magazines
+for(var i = 0; i < magazineData.length; i++){
+
 	var filePath = magazineData[i].fileName;
+
+	var name = magazineData[i].name;
+	var issue = magazineData[i].issue;
+	var author = magazineData[i].author;
+
 	var newFile = $("<a></a>");
 	newFile.attr("href", filePath);
-	var starImgID = "UnavoritedStar.png";
-	if(favorited.indexOf(magazineData[i]) > -1){
-		starImgID = "FavoritedStar.png";
-	}
-	newFile.html("<div class='pdfFile'><img src='ComixIcon2.png' class='iconImg'><p>"+magazineData[i].name+"<br>Issue "+magazineData[i].issue+"<br>By "+magazineData[i].author+"</p><div>");
+	newFile.html("<tr class='newFile'><td><image src='ComixIcon2.png' class='comixIconImg'></td><td><strong>"+name+"</strong></td><td><strong><em>Issue "+issue+"</em></strong></td><td>By "+author+"</td></tr>");
 	newFile.attr("target", "_blank");
 	newFile.attr("rel", "noopener noreferrer");
+
 	var containerDiv = $("<div></div>");
 	containerDiv.addClass("fileContainer");
-	var starBtn = $("<img>");
-	starBtn.attr("src", starImgID);
-	starBtn.addClass("starBtn");
-	starBtn.magazine = magazineData[i];
-	//containerDiv.append(starBtn);
 	containerDiv.append(newFile);
 	$("#magazineContainer").append(containerDiv);
 }
+
+
+
+//Search page functionality
+var magazineTypes = [];
+for(var i = 0; i < magazineData.length; i++){
+	var currentType = magazineData[i].series;
+	if(magazineTypes.indexOf(currentType) === -1){
+		magazineTypes.push(magazineData[i].series);
+	}
+}
+for(var i = 0; i < magazineTypes.length; i++){
+	var newChoice = $("<option></option>");
+	newChoice.html(magazineTypes[i]);
+
+	$(".magazinesChoice").append(newChoice);
+}
+$("#searchBtn").click(function(){
+	$("#magazineContainer").html("");
+	for(var i = 0; i < magazineData.length; i++){
+		console.log(document.querySelector(".magazinesChoice").value);
+		if(magazineData[i].series === document.querySelector(".magazinesChoice").value){
+			var filePath = magazineData[i].fileName;
+
+			var name = magazineData[i].name;
+			var issue = magazineData[i].issue;
+			var author = magazineData[i].author;
+
+			var newFile = $("<a></a>");
+			newFile.attr("href", filePath);
+			newFile.html("<tr class='newFile'><td><image src='ComixIcon2.png' class='comixIconImg'></td><td><strong>"+name+"</strong></td><td><strong><em>Issue "+issue+"</em></strong></td><td>By "+author+"</td></tr>");
+			newFile.attr("target", "_blank");
+			newFile.attr("rel", "noopener noreferrer");
+
+			var containerDiv = $("<div></div>");
+			containerDiv.addClass("fileContainer");
+			containerDiv.append(newFile);
+			$("#magazineContainer").append(containerDiv);
+		}
+	}
+});
